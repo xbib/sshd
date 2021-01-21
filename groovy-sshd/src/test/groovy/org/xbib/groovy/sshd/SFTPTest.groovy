06fc1d2a0e0aae3a894bc769f82f2e87ba9d4aa5
Jörg Prante <joergprante@gmail.com>
2021-01-21 21:47:17 +0100

    update gradle to 6.6.1, groovy to 2.5.12, bouncycastle to 1.68, build scripts

--- groovy-sshd/src/test/groovy/org/xbib/groovy/sshd/SFTPTest.groovy
+++ groovy-sshd/src/test/groovy/org/xbib/groovy/sshd/SFTPTest.groovy
@@ -1,8 +1,7 @@
 package org.xbib.groovy.sshd
 
 import groovy.util.logging.Log4j2
-import org.junit.Ignore
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.xbib.io.sshd.eddsa.EdDSASecurityProvider
 
 import java.nio.file.Files
@@ -17,9 +16,8 @@ class SFTPTest {
     }
 
     @Test
-    @Ignore
     void testSFTP() {
-        SFTP sftp = SFTP.newInstance("sftp://demo.wftpserver.com:2222",[username: 'demo-user', password: 'demo-user'.toCharArray()])
+        SFTP sftp = SFTP.newInstance("sftp://demo.wftpserver.com:2222",[username: 'demo', password: 'demo'.toCharArray()])
         log.info sftp.exists('/')
         sftp.each('/') { Path path ->
             log.info "{} {} {}", path, Files.isDirectory(path), Files.getLastModifiedTime(path)
